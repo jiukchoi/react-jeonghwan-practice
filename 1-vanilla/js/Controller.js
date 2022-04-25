3b6e8c7291f5f050cf0392903516607fc5609526
woogie <jiwook131@gmail.com>
2022-04-25 22:10:14 +0900

    style: Add tab

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,11 +1,13 @@
 export default class Controller {
-  constructor(store, { searchFormView, searchResultView }) {
+  constructor(store, { searchFormView, searchResultView, tabView }) {
     this.store = store;
 
     this.searchFormView = searchFormView;
     this.searchResultView = searchResultView;
+    this.tabView = tabView;
 
     this.subscribeViewEvents();
+    this.render();
   }
 
   subscribeViewEvents() {
@@ -27,10 +29,12 @@ export default class Controller {
 
   render() {
     if (this.store.searchKeyword.length > 0) {
+      this.tabView.hide();
       this.searchResultView.show(this.store.searchResult);
       return;
     }
 
+    this.tabView.show();
     this.searchResultView.hide();
   }
 }
