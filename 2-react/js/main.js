29c89e28aa99bfd94b7236f9517de86f94d003a0
woogie <jiwook131@gmail.com>
2022-05-09 15:11:06 +0900

    소개편

---
+++ 2-react/js/main.js
@@ -0,0 +1,9 @@
+const element = (
+  <header>
+    <h2 className="container">검색</h2>
+  </header>
+);
+
+const container = document.querySelector('#app');
+const root = ReactDOM.createRoot(container);
+root.render(element);
